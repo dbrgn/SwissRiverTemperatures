c010d7f5a50ec70715f3447d9460fc60a3b1ee25
Danilo Bargen <gezuru@gmail.com>
2012-01-06 23:03:36 +0100

    Added new BooleanToVisibilityConverter

---
+++ SwissRiverTemperatures/BooleanToVisibilityConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SwissRiverTemperatures
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var flag = (bool)value;
+            if (parameter != null)
+            {
+                if (bool.Parse((string)parameter))
+                {
+                    flag = !flag;
+                }
+            }
+            return flag ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var back = ((value is Visibility) && (((Visibility)value) == Visibility.Visible));
+            if (parameter != null)
+            {
+                if ((bool)parameter)
+                {
+                    back = !back;
+                }
+            }
+            return back;
+        }
+    }
+}
